cfa307b6a7887b7dc6997eda9fc3d3b9b2ae7a16
Syduck333 <smccreary@ftstudent.org>
2026-04-27 13:56:55 -0400

    Training Mode

--- lava.gd
+++ lava.gd
@@ -2,7 +2,7 @@ extends StaticBody3D
 class_name Lava
 
 @onready var Lava: StaticBody3D 
-var rise_speed: float = 1
+var rise_speed: float = 0.0
 	
 func _process(delta):
 	position.y += rise_speed * delta
